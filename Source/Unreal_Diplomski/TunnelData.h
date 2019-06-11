a90a897802217b0ac580b1b00e1c8e9856d3766c
Aleksandar Rajic <rajicaleksandar6@gmail.com>
2019-06-11 13:09:11 +0200

    Dodat novi preaset za rig, sa nosi usta, dodat TunnelData sa nosem, pomeraj na manju toleranciju, i to je to

---
+++ Source/Unreal_Diplomski/TunnelData.h
@@ -0,0 +1,27 @@
+
+#ifndef TUNNEL_DATA_H
+#define TUNNEL_DATA_H
+
+
+class TunnelData {
+public:
+
+// Root
+
+	int noseroot;
+
+// eyebrow
+
+	int r_eyebrow_out;
+	int r_eyebrow_in;
+
+	int l_eyebrow_out;
+	int l_eyebrow_in;
+
+
+
+
+};
+
+
+#endif
